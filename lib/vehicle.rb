406729a18c4dcb9c281bee5e786d21b06721039e
ChrisDemahy <demahyc@gmail.com>
2020-10-14 13:30:08 -0500

    finished

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,40 @@
+# Describes an Vehicle
 class Vehicle
+  ### CLASS DEFINITIONS ###
 
+
+  # Class variables
+  @@all = [] # list of all Vehicles
+
+  # Reader for all class variable
+  def self.all
+    @@all
+  end  
+
+
+  ### INSTANCE DEFINITIONS ###
+
+  # instance readers and writers
+  attr_accessor :wheel_size, :wheel_number
+
+  # default constructor
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+    self.save
+  end
+
+  # saves Vehicle instance to all class variable
+  def save
+    self.class.all << self
+  end
+
+  # instance methods #
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end 
 end
